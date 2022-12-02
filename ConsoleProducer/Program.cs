fea6bc47eee75ae1c722aa58f68519ae4104a27f
Janardhana Bandaru <jana4al@gmail.com>
2022-12-02 06:55:33 +0530

    Modified the code.

--- ConsoleProducer/Program.cs
+++ ConsoleProducer/Program.cs
@@ -3,12 +3,12 @@ using RabbitMQ.Client;
 
 Console.WriteLine("Hello RabbitMQ... Let's play with you... :)");
 
-var hostname = "b-8c289b1b-5f53-44ef-bc94-fd39d396a2f9.mq.us-east-1.amazonaws.com";
+var hostname = "b-*******.amazonaws.com";
 var factory = new ConnectionFactory()
 {
     HostName = hostname,
-    UserName = "channelsmart",
-    Password = "EAQKvSWRm3kDBkKT",
+    UserName = "***",
+    Password = "****",
     Port = 5671,
     VirtualHost = "/",
     Ssl = new SslOption() { Enabled = true, ServerName = hostname }
@@ -17,7 +17,7 @@ var factory = new ConnectionFactory()
 
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
-var queueName = "jlqueue";
+var queueName = "jana-first-queue";
 
 channel.QueueDeclare(
     queue: queueName,
