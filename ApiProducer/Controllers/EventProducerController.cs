55dc9ddb7783b33988f10fb892ee376dcfc991ce
Janardhana Bandaru <jana4al@gmail.com>
2022-12-02 06:48:36 +0530

    Added code to connect to aws native rabbitmq

---
+++ ApiProducer/Controllers/EventProducerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProducer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventProducerController : ControllerBase
+    {
+        [HttpPost("produce")]
+        public ActionResult Publish()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ConsoleProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello RabbitMQ... Let's play with you... :)");
+
+var hostname = "b-8c289b1b-5f53-44ef-bc94-fd39d396a2f9.mq.us-east-1.amazonaws.com";
+var factory = new ConnectionFactory()
+{
+    HostName = hostname,
+    UserName = "channelsmart",
+    Password = "EAQKvSWRm3kDBkKT",
+    Port = 5671,
+    VirtualHost = "/",
+    Ssl = new SslOption() { Enabled = true, ServerName = hostname }
+};
+
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+var queueName = "jlqueue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    arguments: null
+);
+
+var message = "This is my first message";
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish("", queueName, null, encodedMessage);
+Console.WriteLine($"Message is published and the message is {message}");
